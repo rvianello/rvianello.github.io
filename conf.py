b513ed8b503a366f15c5ba84ab094a9c3cf92ad5
Riccardo Vianello <riccardo.vianello@gmail.com>
2021-04-25 09:14:45 +0000

    Nikola auto commit.
    Nikola version: 8.1.2

--- conf.py
+++ conf.py
@@ -17,7 +17,7 @@ import time
 
 # Data about this site
 BLOG_AUTHOR = "Riccardo Vianello"  # (translatable)
-BLOG_TITLE = "This space intentionally left blank"  # (translatable)
+BLOG_TITLE = "De lana caprina"  # (translatable)
 # This is the main URL for your site. It will be used
 # in a prominent link. Don't forget the protocol (http/https)!
 SITE_URL = "https://rvianello.github.io/"
